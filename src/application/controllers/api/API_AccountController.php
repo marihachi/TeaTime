f41fabb465b88a0d9a03d61ac1fb158b88e40b97
marihachi <marihachi0620@gmail.com>
2015-08-03 03:39:57 +0900

    Add Content-Type

--- src/application/controllers/api/API_AccountController.php
+++ src/application/controllers/api/API_AccountController.php
@@ -9,6 +9,8 @@ class API_AccountController extends CI_Controller
 
 		if (preg_match('/^(json)$/i', $extention) === 1)
 		{
+			header("Content-Type: application/json; charset=utf-8");
+			
 			$post = $this->input->post();
 			if (array_key_exists('screen_name', $post) && array_key_exists('password', $post) && array_key_exists('name', $post) && array_key_exists('bio', $post))
 			{
@@ -29,7 +31,7 @@ class API_AccountController extends CI_Controller
 					else
 					{
 						http_response_code(500);
-						$info['error']['code'] = 200;
+						$info['error']['code'] = 103;
 						$info['error']['message'] = 'Failed to execute.';
 					}
 				}
