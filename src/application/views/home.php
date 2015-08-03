79a62fb5b06fc96e5636f4ccee2129030ecea819
marihachi <marihachi0620@gmail.com>
2015-08-04 04:52:32 +0900

    Add logout button

--- src/application/views/home.php
+++ src/application/views/home.php
@@ -4,7 +4,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="jp">
 	<head>
 		<meta charset=UTF-8>
+		<meta http-equiv=X-UA-Compatible content="IE=edge" />
+		<meta name=viewport content="width=device-width, initial-scale=1" />
 		<title>ホーム - TeaTime | ティータイムにピッタリなSNS</title>
+		<link href=//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css rel=stylesheet />
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<script src=//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js></script>
+		<script src=//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js></script>
+		<script>
+			$(function() {
+				$('#ToolBar button').onclick(function(event) {
+					event.preventDefault();
+					$.ajax("http://marihachi.php.xdomain.jp/tea-time/api/web/account/logout", {
+						type: 'get',
+						dataType: 'json',
+						xhrFields: {
+							withCredentials: true
+						}
+					}).done(function(){
+						location.href = "http://marihachi.php.xdomain.jp/tea-time/";
+					}).fail(function(){
+						$('#logout-message').text("ログアウトに失敗しました。");
+					});
+				});
+			});
+		</script>
 		<style>
 			* {
 				margin: 0;
@@ -37,11 +64,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				<li>Mention</li>
 				<li>
 					<form>
-						<input type=text id=TextLineBox></input>
-						<input type=submit value=Post></input>
+						<input type=text id=TextLineBox>
+						<input type=submit value=Post>
 					</form>
 				</li>
 			</ul>
+			<button>ログアウト</button>
+			<label id=logout-message></label>
 		</nav>
 		<p>TeaTimeへようこそ</p>
 		<p>ユーザーリスト</p>
