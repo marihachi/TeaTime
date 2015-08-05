2394204615bfce450722649848eec1d9ba7eaffb
marihachi <marihachi0620@gmail.com>
2015-08-05 15:33:42 +0900

    ud

--- src/application/views/home.php
+++ src/application/views/home.php
@@ -36,20 +36,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			a:active, a:hover, a:focus {
 				color: #ddd;
 			}
-			body {
-				background: #eee;
+			.jumbotron {
 				color: #333;
 				padding-top: 94px;
+				
+				background: url("/tea-time/bg002.jpg");
+				background-position: center center;
+				background-size: cover;
+				background-repeat: no-repeat;
+				background-attachment: fixed;
 			}
-			.btn-teatime-1 {
+			.home-nav .btn-teatime-white {
 				background: transparent;
 				border: 1px solid #fff;
 				color: #fff;
+				border: 2px solid rgb(255, 255, 255);
+				margin: 6px 0;
 			}
 			.btn-teatime-1:hover,
 			.btn-teatime-1:active,
 			.btn-teatime-1:focus{
 				color: #eee;
+				outline: none;
 			}
 			
 			.home-nav {
@@ -58,50 +66,85 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				right: 0px;
 				left: 0px;
 				z-index: 1030;
-				padding: 6px;
-				
 				color: #fff;
 				background: #D59B6D;
-				opacity: 0.92;
+				background-color: rgba(255, 173, 67, 0.62);
 			}
-			.home-nav a {
+			.home-nav a.item {
+				display: block;
 				font-size: 16px;
+				padding: 12px;
+				text-align: center;
 			}
-			.form-control {
+			.home-nav input.form-control {
 				color: #555;
 				background-color: rgba(255, 255, 255, 0.6);
 				box-shadow: none;
+				border: 2px solid rgb(255, 255, 255);
+				height: 36px;
+				margin: 6px 0;
 			}
-			.form-control:focus {
+			.home-nav input.form-control:focus {
 				border-color: #FF9A00;
 				box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075),0 0 4px rgba(251, 152, 0, 0.6);
 			}
+			.no-padding-right {
+				padding-right: 0;
+			}
+			.no-padding-left {
+				padding-left: 0;
+			}
+			.entry {
+				border: 2px solid white;
+				border-radius: 5px;
+				margin: 10px;
+				padding: 10px;
+				color: #fff;
+				background-color: rgba(255, 173, 67, 0.62);
+			}
+			.entry img {
+				height: 64px;
+				width: 64px;
+			}
 		</style>
 	</head>
-	<body>
+	<body class=jumbotron>
 		<nav class="home-nav">
 			<div class=container>
 				<div class=row>
-					<div class="col-xs-4 row">
-						<div class="col-xs-6"><a href="#">Home</a></div>
-						<div class="col-xs-6"><a href="#">Mention</a></div>
+					<div class="col-xs-4">
+						<div class="col-xs-6 no-padding-right"><a href="#" class=item>Home</a></div>
+						<div class="col-xs-6 no-padding-left"><a href="#" class=item>Mention</a></div>
 					</div>
 					<div class="col-xs-6">
 						<div class=input-group>
 							<input type=text class="form-control" id=post-text-box>
 							<span class="input-group-btn">
-								<button class="btn btn-teatime-1" id=post-button>Post</button>
+								<button class="btn btn-teatime-white" id=post-button>Post</button>
 							</span>
 						</div>
 					</div>
 					<div class="col-xs-2">
-						<button id=logout-button class="btn btn-default">ログアウト</button>
+						<button id=logout-button class="btn btn-teatime-white">ログアウト</button>
 					</div>
 				</div>
 			</div>
 		</nav>
 		<main>
-			
+			<div class=container>
+				<div class=row>
+					<div class="entry col-xs-4">
+						<img src="/tea-time/icon_test.jpg" />
+						This is status sample.
+					</div>
+				</div>
+				<div class=row>
+					<div class="entry col-xs-4">
+						<img src="/tea-time/icon_test.jpg" />
+						This is status sample.
+					</div>
+				</div>
+			</div>
 		</main>
 	</body>
 </html>
