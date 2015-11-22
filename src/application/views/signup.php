3bac996ca2a676da669cab3a099c4dc17d3290be
Marihachi <marihachi0620@gmail.com>
2015-11-22 23:37:21 +0900

    update signup view

--- src/application/views/signup.php
+++ src/application/views/signup.php
@@ -4,21 +4,66 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="jp">
 	<head>
 		<meta charset=UTF-8>
+		<meta http-equiv=X-UA-Compatible content="IE=edge" />
+		<meta name=viewport content="width=device-width, initial-scale=1" />
 		<title>新しいアカウントを作成 - TeaTime | ティータイムにピッタリなSNS</title>
+		<script src=//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js></script>
 		<style>
+			body * {
+				box-sizing: border-box;
+			}
 			body {
 				background: #D59B6D;
 				color: #fff;
+				background: url("/tea-time/bg002.jpg");
+				background-position: center center;
+				background-size: cover;
+				background-repeat: no-repeat;
+				background-attachment: fixed;
 			}
 			main {
-				margin: auto;
-				position: absolute;
-				top: 0px;
-				right: 0px;
-				bottom: 0px;
-				left: 0px;
-				height: 14em;
 				text-align: center;
+				display: flex;
+			}
+			main > section {
+				background-color: rgba(255, 173, 67, 0.62);
+				margin: 10px;
+				padding: 20px;
+				border-radius: 5px;
+				color: #fff;
+			}
+			#account-create-section {
+				margin: 0 auto;
+				width: 500px;
+			}
+			#account-create-form {
+				margin: 0 auto;
+				width: 400px;
+			}
+			#account-create-form > label {
+				display: block;
+				margin: 10px 0px;
+			}
+			#account-create-form input, #account-create-form textarea {
+				color: rgba(255, 255, 255, 1);
+				border: 1px solid rgba(255, 255, 255, 1);
+				background-color: transparent;
+			}
+			
+			#account-create-form > label > input, #account-create-form > label > textarea {
+				padding: 8px;
+				width: 100%;
+				resize: vertical;
+			}
+			#account-create-form > div {
+				text-align: right;
+			}
+			#account-create-form > div > input {
+				width: 100px;
+				height: 35px;
+			}
+			#account-create-form > div > input:hover {
+				background-color: rgba(255, 255, 255, .4);
 			}
 		</style>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
@@ -36,7 +81,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					}).done(function(){
 						location.href = "http://marihachi.php.xdomain.jp/tea-time";
 					}).fail(function(){
-
+						$("#message").text("サインアップに失敗しました。入力情報を確認してください。");
 					});
 				});
 			});
@@ -44,14 +89,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</head>
 	<body>
 		<main>
-			<form id="account-create-form">
-				<input type="text" placeholder="ユーザー名" name="screen_name" />
-				<input type="password" placeholder="パスワード" name="password" />
-				<input type="text" placeholder="名前" name="name" />
-				<input type="text" placeholder="プロフィール" name="bio" />
-				<input type="submit" />
-			</form>
-			<p><a href=/tea-time>エントランスへ</a></p>
+			<section id="account-create-section">
+				<h1>さあTeaTimeをはじめましょう</h1>
+				<form id="account-create-form">
+					<label>
+						<input type="text" placeholder="ユーザー名" name="screen_name">
+					</label>
+					<label>
+						<input type="password" placeholder="パスワード" name="password">
+					</label>
+					<label>
+						<input type="text" placeholder="名前" name="name">
+					</label>
+					<label>
+						<textarea placeholder="プロフィール" name="bio"></textarea>
+					</label>
+					<div>
+						<input type="submit" value="登録">
+					</div>
+				</form>
+				<span id="message"></span>
+				<p><a href=/tea-time>エントランスへ</a></p>
+			</section>
 		</main>
 	</body>
 </html>
